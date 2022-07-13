af896f98eda23b44a18dbc4b67f61cc124b90203
Pluto Chan <pumsscco@163.com>
2022-07-13 16:35:44 +0800

    完成了最新记录与最近一周页面

--- attendance/attn.py
+++ attendance/attn.py
@@ -2,10 +2,33 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+#----先把表单相关的模块加入进来，后面会用到的
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+#----end
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YHXPUNEK4TCVJHVD9P97CE3WQRAARPXX'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://flask:bTzxVYVt5PQT2SgsJ4JzjNEW@mysql/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db=SQLAlchemy(app)
+
+class Attendance(db.Model):
+    __tablename__ = 'attendance'
+
+    id = db.Column(db.Integer, primary_key=True)
+    checkin = db.Column(db.DateTime, nullable=False, unique=True, info='打上班卡时间')
+    checkout = db.Column(db.DateTime, nullable=False, unique=True, info='打下班卡时间')
+    comments = db.Column(db.String(256), server_default=db.FetchedValue(), info='备注')
+    
+    def __repr__(self):
+        return '<<考勤记录： %r, %r, %r >>' % (self.checkin, self.checkout, self.comments)
 
 @app.route('/')
 def index():
@@ -13,12 +36,17 @@ def index():
 
 @app.route('/rec/latest')
 def latest():
+    rec=Attendance.query.order_by(Attendance.checkin.desc()).first()
     return render_template('rec/latest.html', rec=rec)
 
 @app.route('/rec/last-week')
 def last_week():
+    NOW=datetime.now()
+    recs=Attendance.query.filter(Attendance.checkin>=NOW-timedelta(days=7)).order_by(Attendance.checkin.desc()).all()
     return render_template('rec/last-week.html', recs=recs)
 
 @app.route('/rec/last-month')
 def last_month():
+    NOW=datetime.now()
+    recs=Attendance.query.filter(Attendance.checkin>=NOW-timedelta(days=7)).order_by(Attendance.checkin.desc()).all()
     return render_template('rec/last-month.html', recs=recs)
