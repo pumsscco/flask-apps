71a604b7e94d4e7f896d6d782cde13472d6452e4
Pluto Chan <pumsscco@163.com>
2022-08-05 18:14:58 +0800

    复杂查询用sqlalchemy也很难表示！

--- attendance/attn.py
+++ attendance/attn.py
@@ -9,12 +9,14 @@ from wtforms.validators import DataRequired
 #----end
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from sqlalchemy.sql import func
 from datetime import date, datetime, timedelta
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YHXPUNEK4TCVJHVD9P97CE3WQRAARPXX'
 app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://flask:bTzxVYVt5PQT2SgsJ4JzjNEW@mysql/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -32,10 +34,13 @@ class Attendance(db.Model):
         return '<<考勤记录： %r, %r, %r >>' % (self.checkin, self.checkout, self.comments)
     
     def to_json(self):
+        checkin_key='打上班卡时间'
+        checkout_key='打下班卡时间'
+        comments_key='备注'
         json_attn = {
-            u'打上班卡时间': self.checkin,
-            u'打下班卡时间': self.checkout,
-            u'备注': self.comments
+            checkin_key: self.checkin.strftime('%Y年%m月%d日 %H时%M分%S秒'),
+            checkout_key: self.checkout.strftime('%Y年%m月%d日 %H时%M分%S秒'),
+            comments_key: self.comments
         }
         return json_attn
 @app.route('/')
@@ -52,21 +57,65 @@ def api_latest():
     rec=Attendance.query.order_by(Attendance.checkin.desc()).first()
     return jsonify(rec.to_json())
 
-@app.route('/rec/last-week')
 def last_week():
     NOW=datetime.now()
     recs=Attendance.query.filter(
         Attendance.checkin>NOW-timedelta(days=7)).order_by(
         Attendance.checkin.desc()).all()
+    return recs
+
+@app.route('/rec/last-week')
+def web_last_week():
+    recs=last_week()
     return render_template('rec/last-week.html', recs=recs)
 
-@app.route('/rec/last-month')
+@app.route('/api/rec/last-week')
+def api_last_week():
+    recs=last_week()
+    json_list=[rec.to_json() for rec in recs]
+    return jsonify(json_list)
+
+#近一个月记录
 def last_month():
-    #NOW=datetime.now()
-    page = request.args.get('page', 1, type=int)
-    pagination=Attendance.query.filter(
+    recs=Attendance.query.filter(
         Attendance.checkin>text('NOW() - INTERVAL 1 MONTH')).order_by(
-        Attendance.checkin.desc()).paginate(
-        page, per_page=5, error_out=False)
+        Attendance.checkin.desc())
+    return recs
+##直接输出到网页
+@app.route('/rec/last-month')
+def web_last_month():
+    res=last_month()
+    page = request.args.get('page', 1, type=int)
+    pagination=res.paginate(page, per_page=5, error_out=False)
     recs = pagination.items
     return render_template('rec/last-month.html', recs=recs,pagination=pagination)
+##输出为json，供api调用
+@app.route('/api/rec/last-month')
+def api_last_month():
+    recs=last_month().all()
+    json_list=[rec.to_json() for rec in recs]
+    return jsonify(json_list)
+
+'''SELECT year(checkin),month(checkin),
+        sum(hour(timediff(checkout,checkin))+minute(timediff(checkout,checkin))/60)
+        FROM attendance group by year(checkin),month(checkin)'''
+
+@app.route('/api/stat/month')
+def month_hour():
+    month_stats=Attendance.query(
+        func.year(Attendance.checkin), 
+        func.month(Attendance.checkin), 
+        func.sum(
+            func.hour(
+                func.timediff(Attendance.checkout, Attendance.checkin)
+            )+func.minute(
+                func.timediff(Attendance.checkout, Attendance.checkin)
+            )
+        ) / 60
+    ).group_by(
+        func.year(Attendance.checkin), 
+        func.month(Attendance.checkout)
+    )
+    json_list=[month_stat.to_json() for month_stat in month_stats]
+    return jsonify(json_list)
+
